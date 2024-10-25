3d71c686b174f4d9807c995b308dfa099700dd63
yiyuyan <yiyuyan>
2024-10-25 19:56:56 +0800

    fix

--- src/main/java/cn/ksmcbrigade/IIS/mixin/ItemInfinityMixin.java
+++ src/main/java/cn/ksmcbrigade/IIS/mixin/ItemInfinityMixin.java
@@ -17,8 +17,8 @@ public class ItemInfinityMixin {
     public void inventoryTick(ItemStack p_41404_, Level p_41405_, Entity p_41406_, int p_41407_, boolean p_41408_, CallbackInfo ci) {
         if(!p_41404_.isEmpty() && !(p_41404_.getItem() instanceof BlockItem) && InfinityItems.HAS(p_41404_,p_41405_)){
             p_41404_.setDamageValue(0);
-            if(p_41404_.getCount()==2 && p_41404_.getMaxStackSize()!=1){
-                p_41404_.setCount(3);
+            if(p_41404_.getCount()==1 && p_41404_.getMaxStackSize()!=1){
+                p_41404_.setCount(2);
             }
         }
     }
