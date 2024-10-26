adac09c62abbd44c2fbade31d4254adb7c273c52
yiyuyan <yiyuyan>
2024-10-26 20:28:46 +0800

    fix,fix,fix!!!!!!

--- src/main/java/cn/ksmcbrigade/IIS/mixin/BlockItemInfinityMixin.java
+++ src/main/java/cn/ksmcbrigade/IIS/mixin/BlockItemInfinityMixin.java
@@ -1,25 +1,25 @@
 package cn.ksmcbrigade.IIS.mixin;
 
 import cn.ksmcbrigade.IIS.InfinityItems;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.context.UseOnContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockItem.class)
-public class BlockItemInfinityMixin {
-    @Inject(method = "useOn",at = @At("RETURN"))
-    public void useOn(UseOnContext p_40581_, CallbackInfoReturnable<InteractionResult> cir) {
-        ItemStack ITEM = p_40581_.getItemInHand();
-        Player player = p_40581_.getPlayer();
-        if(cir.getReturnValue().consumesAction() && player!=null && !player.isCreative() && InfinityItems.HAS(ITEM,p_40581_.getLevel()) && !ITEM.getItem().equals(Items.POWDER_SNOW_BUCKET) && ITEM.getCount()<=1){
-            ITEM.setCount(ITEM.getCount()+1);
+public abstract class BlockItemInfinityMixin extends Item {
+    public BlockItemInfinityMixin(Properties p_41383_) {
+        super(p_41383_);
+    }
+
+    @Redirect(method = "place",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;consume(ILnet/minecraft/world/entity/LivingEntity;)V"))
+    public void useOn(ItemStack ITEM, int p_326311_, LivingEntity p_326200_) {
+        if(!InfinityItems.HAS(ITEM,p_326200_.level()) || ITEM.getItem().equals(Items.POWDER_SNOW_BUCKET) || ITEM.getCount()>1){
+            ITEM.consume(p_326311_,p_326200_);
         }
     }
 }
--- src/main/java/cn/ksmcbrigade/IIS/mixin/ItemInfinityMixin.java
+++ src/main/java/cn/ksmcbrigade/IIS/mixin/ItemInfinityMixin.java
@@ -2,7 +2,6 @@ package cn.ksmcbrigade.IIS.mixin;
 
 import cn.ksmcbrigade.IIS.InfinityItems;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ItemInfinityMixin {
     @Inject(method = "inventoryTick",at = @At("TAIL"))
     public void inventoryTick(ItemStack p_41404_, Level p_41405_, Entity p_41406_, int p_41407_, boolean p_41408_, CallbackInfo ci) {
-        if(!p_41404_.isEmpty() && !(p_41404_.getItem() instanceof BlockItem) && InfinityItems.HAS(p_41404_,p_41405_)){
+        if(!p_41404_.isEmpty() && InfinityItems.HAS(p_41404_,p_41405_)){
             p_41404_.setDamageValue(0);
             if(p_41404_.getCount()==1 && p_41404_.getMaxStackSize()!=1){
                 p_41404_.setCount(2);
